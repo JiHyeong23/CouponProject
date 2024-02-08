02aa2b24cc0e4aa399732771e2745c7484e148a0
JiHyeong23 <ekdl1120@gmail.com>
2024-02-08 23:46:50 +0900

    Fit: Filter

--- src/main/java/msa/gateway/AuthorizationHeaderFilter.java
+++ src/main/java/msa/gateway/AuthorizationHeaderFilter.java
@@ -1,13 +1,41 @@
 package msa.gateway;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.stereotype.Component;
 
 @Component
-public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<AbstractGatewayFilterFactory.NameConfig> {
+public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<AuthorizationHeaderFilter.Config> {
+    @Autowired
+    private JwtHelper jwtHelper;
+    public AuthorizationHeaderFilter() {
+        super(Config.class);
+    }
+
     @Override
-    public GatewayFilter apply(NameConfig config) {
-        return null;
+    public GatewayFilter apply(Config config) {
+        return ((exchange, chain) -> {
+            String getHeader = exchange.getRequest().getHeaders().get("Authorization").get(0);
+            String token = getHeader.substring(7);
+            String index = jwtHelper.getEmailFromJwtToken(token);
+
+//            Consumer<HttpHeaders> headers = new Consumer<HttpHeaders>() {
+//                @Override
+//                public void accept(HttpHeaders httpHeaders) {
+//                    httpHeaders.add("Authorization", getHeader);
+//                    httpHeaders.add("Authorization-Id", index);
+//                }
+//            };
+
+            //exchange.getRequest().mutate().headers(headers).build();
+            exchange.getRequest().mutate().header("Authorization-Id", index).build();
+
+            return  chain.filter(exchange);
+        });
+    }
+
+    public static class Config{
+
     }
 }

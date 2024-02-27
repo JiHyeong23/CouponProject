e0d840702b9593a6eb663aa776633cd86992da4c
JiHyeong23 <ekdl1120@gmail.com>
2024-02-27 22:04:30 +0900

    Feat: UserCoupon Entity

---
+++ src/main/java/msa/ShopService/userCoupon/UserCoupon.java
@@ -0,0 +1,28 @@
+package msa.ShopService.userCoupon;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserCoupon {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long userId;
+    private String couponCode;
+    @Builder.Default
+    private LocalDateTime time = LocalDateTime.now();
+}

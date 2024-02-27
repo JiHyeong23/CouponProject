18acdd47c4ec710c0ae2bb53cda9151bec9ae774
JiHyeong23 <ekdl1120@gmail.com>
2024-02-27 22:05:12 +0900

    Feat: UserCoupon Repository

---
+++ src/main/java/msa/ShopService/userCoupon/UserCouponRepository.java
@@ -0,0 +1,8 @@
+package msa.ShopService.userCoupon;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserCouponRepository extends JpaRepository<UserCoupon, Long> {
+}
